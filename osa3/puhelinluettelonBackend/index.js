f3934fb171c9efe4b8c8f43178e20268d6640336
juuvuor <96438568+juuvuor@users.noreply.github.com>
2023-10-12 16:43:20 +0300

    uibuild

--- osa3/puhelinluettelonBackend/index.js
+++ osa3/puhelinluettelonBackend/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :res[content-length] — :response-time ms :body'));
 const cors = require('cors')
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] — :response-time ms :body'));
 app.use(cors())
 app.use(express.static('dist'))
 
