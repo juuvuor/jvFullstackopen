764047046fb4ca740a1c8671b919e6ddd8bdbee0
juuvuor <96438568+juuvuor@users.noreply.github.com>
2023-10-25 08:53:00 +0300

    viikko 3 valmis

--- osa3/puhelinluettelonBackend/index.js
+++ osa3/puhelinluettelonBackend/index.js
@@ -1,49 +1,49 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
-morgan.token("body", (req, res) => JSON.stringify(req.body));
-const cors = require("cors");
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+morgan.token('body', (req) => JSON.stringify(req.body))
+const cors = require('cors')
 
 
 
-const Person = require("./models/person");
+const Person = require('./models/person')
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: "unknown endpoint" });
-  };
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 
 
 
-app.use(express.static("build"));
-app.use(express.json());
+app.use(express.static('build'))
+app.use(express.json())
 app.use(
-  morgan(":method :url :status :res[content-length] — :response-time ms :body")
-);
-app.use(cors());
+  morgan(':method :url :status :res[content-length] — :response-time ms :body')
+)
+app.use(cors())
 
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
   const person = new Person({
     name: body.name,
     number: body.number,
     date: new Date().toString(),
-  });
+  })
 
   person.save()
     .then(savedPerson => {
@@ -53,58 +53,58 @@ app.post("/api/persons", (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then((person) => {
-    response.json(person);
-  });
-});
+    response.json(person)
+  })
+})
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end;
+        response.status(404).end
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-const countContacts = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId;
-};
+// const countContacts = () => {
+//   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0
+//   return maxId
+// }
 
-app.get("/info", (req, res) => {
-  //   const persons = countContacts();
-  const time = new Date();
-  console.log("time: ", time);
-  res.send(`
-        <div>
-            <p> Phonebook has info for ${persons} people </p>
-            <p> ${time} </p>
-        </div>
-        `);
-});
+// app.get('/info', (req, res) => {
+//   //   const persons = countContacts();
+//   const time = new Date()
+//   console.log('time: ', time)
+//   res.send(`
+//         <div>
+//             <p> Phonebook has info for ${persons} people </p>
+//             <p> ${time} </p>
+//         </div>
+//         `)
+// })
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then((result) => {
-      console.log("result: ", result);
-      response.status(204).end();
+      console.log('result: ', result)
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.use(unknownEndpoint);
-app.use(errorHandler);
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- osa3/puhelinluettelonBackend/models/person.js
+++ osa3/puhelinluettelonBackend/models/person.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+// console.log('connecting to', url)
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 
 const personSchema = new mongoose.Schema({
@@ -23,15 +23,15 @@ const personSchema = new mongoose.Schema({
   },
   number: String,
   date: String,
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     // console.log('käydäännkö me täällä')
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
