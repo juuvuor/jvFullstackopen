0f6fb68be5d85250bf0dc116d34a0e438578f43c
juuvuor <96438568+juuvuor@users.noreply.github.com>
2023-11-02 09:09:24 +0200

    4 osa alulle

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,23 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const http = require("http");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
